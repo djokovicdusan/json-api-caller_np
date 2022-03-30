d170f4959e8c8eddce75e35dd435f3f046c42ded
ddjok <ddjokovic9@gmail.com>
2022-03-30 16:43:36 +0200

    Dodat i dokumentovan model

---
+++ src/main/java/rs/ac/bg/fon/np/mvn_jsonApiCaller/domain/Location.java
@@ -0,0 +1,44 @@
+package rs.ac.bg.fon.np.mvn_jsonApiCaller.domain;
+/**
+ * Klasa predstavlja model serverskog odgovora koji predstavlja lokaciju i blizi opis mernih stanica polena u Republici Srbiji
+ * @author ddjok
+ *
+ */
+
+
+public class Location {
+	private int id;
+	private String name;
+	private String description;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	@Override
+	public String toString() {
+		return "Location [name=" + name + ", description=" + description + "]";
+	}
+	
+	
+	
+	     
+	
+	
+	
+
+}
